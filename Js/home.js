700a2f390686f99453e5d4f947432a6e574c7495
Zahra13s <122019063+Zahra13s@users.noreply.github.com>
2024-05-06 21:20:21 +0630

    edit

--- Js/home.js
+++ Js/home.js
@@ -1,6 +1,6 @@
 let body = document.getElementById("body");
 let arrow = document.getElementById("arrow");
-let side_nav = document.getElementById("side_nav");
+const sidebar = document.querySelector('aside');
 let main = document.getElementById("main");
 let achievement = document.getElementById("achievement");
 
@@ -8,16 +8,8 @@ arrow.addEventListener("click", side_nav_toggle);
 // arrow.addEventListener("onclick", side_nav_toggle()); -> wrong
 
 function side_nav_toggle() {
-      // Toggle the visibility of the side nav
-      sideNav.style.display = sideNav.style.display === 'none' ? 'block' : 'none';
-        
-      // Adjust grid template areas based on side nav visibility
-      if (sideNav.style.display === 'none') {
-        body.style.gridTemplateAreas = "arrow main achievements";
-      } else {
-          mainContent.style.gridTemplateAreas = '"side_nav arrow_toggle main achievements"';
-      }
-    
+  
+
 
 }
 
